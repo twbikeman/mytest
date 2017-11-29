bb7125e73b0136d0e286187a986eef8ac31a3bdf
Bryan Tsai <bikeman@mail2000.com.tw>
2017-11-29 15:27:53 +0800

    hello2.cpp is SDL

---
+++ hello2.cpp
@@ -0,0 +1,48 @@
+#include <SDL2/SDL.h>
+
+SDL_Window* g_pWindow = 0;
+SDL_Renderer* g_pRenderer = 0;
+
+bool init(const char* title, int xpos, int ypos, int height, int width, int flags);
+
+
+
+int main(int argc, char* args[])
+{
+	if(SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+	{
+		g_pWindow = SDL_CreateWindow("Chapter 1: Setting up SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+		if(g_pWindow != 0)
+		{
+			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+		}
+	
+	}
+	else 
+	{
+		return 1;
+	}
+	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
+	SDL_RenderClear(g_pRenderer);
+	SDL_RenderPresent(g_pRenderer);
+	SDL_Delay(5000);
+	SDL_Quit();
+	return 0;
+}
+
+bool init(const char* title, int xpos, int ypos, int height, int width, int flags)
+{
+	if(SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+	{
+		g_pWindow = SDL_CreateWindow(title, xpos, ypos, height, width, flags);
+		if(g_pWindow != 0)
+		{
+			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+		}
+	}
+	else
+	{
+		return false;
+	}
+	return true;
+}
